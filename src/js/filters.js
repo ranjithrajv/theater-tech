3d7e0027440e597b4d29d63e234b9db3dd7e38e9
ranjithraj <ranjithraj@swecha.net>
2026-07-03 17:49:56 +0530

    dedup city filter

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,7 +1,6 @@
 class FilterManager {
     constructor() {
         this.filters = {
-            city: [],
             plf_format: [],
             projection_type: [],
             sound_format: [],
@@ -39,9 +38,6 @@ class FilterManager {
     collectFilterValues() {
         if (!this.container) return;
 
-        const cityEls = this.container.querySelectorAll('input[name="city"]:checked');
-        this.filters.city = Array.from(cityEls).map(el => el.value);
-
         const plfEls = this.container.querySelectorAll('input[name="plf_format"]:checked');
         this.filters.plf_format = Array.from(plfEls).map(el => el.value);
 
@@ -63,9 +59,6 @@ class FilterManager {
 
     applyFilters() {
         this.filteredScreens = this.allScreens.filter(screen => {
-            if (this.filters.city.length > 0 &&
-                !this.filters.city.includes(screen.city)) return false;
-
             if (this.filters.plf_format.length > 0 &&
                 !this.filters.plf_format.includes(screen.plf_format)) return false;
 
@@ -140,7 +133,6 @@ class FilterManager {
     render() {
         if (!this.container) return;
 
-        const cities = this.getUniqueValues('city');
         const plfFormats = this.getUniqueValues('plf_format');
         const projTypes = this.getUniqueValues('projection_type');
         const soundFormats = this.getUniqueValues('sound_format');
@@ -148,17 +140,6 @@ class FilterManager {
         this.container.innerHTML = `
             <div class="filter-bar">
                 <div class="filter-row">
-                    <div class="filter-group">
-                        <label class="filter-group-label">City</label>
-                        <div class="filter-checkboxes">
-                            ${cities.map(f => `
-                                <label class="filter-checkbox">
-                                    <input type="checkbox" name="city" value="${f}" />
-                                    <span>${f}</span>
-                                </label>
-                            `).join('')}
-                        </div>
-                    </div>
                     <div class="filter-group">
                         <label class="filter-group-label">Search</label>
                         <input type="text" id="filter-search" class="filter-input" placeholder="Search theaters..." />
